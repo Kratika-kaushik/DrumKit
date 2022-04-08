0e90a026ecaa89301cdc6d047885c990ba682b49
Kratika Kaushik <72157581+Kratika-kaushik@users.noreply.github.com>
2022-04-08 19:22:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var numberofkeys = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberofkeys; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimationn(buttonInnerHTML);
+  });
+}
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimationn(event.key);
+});
+function buttonAnimationn(currentKey){
+  var activeButton= document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+
+}
+function makeSound(key){
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        default:
+          console.log(this.innerHTML);
+          break;
+      }
+}
